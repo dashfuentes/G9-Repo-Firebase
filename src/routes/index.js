11be300813ea8d583c69ef95db197c5c1973bc06
dashfuentes <smis117112@gmail.com>
2022-03-22 20:03:44 -0600

    delete firebase and rebuild the get function

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,22 +12,48 @@ admin.initializeApp({
 
 const db = admin.database();
 
-router.get( '/', async( req, res ) => {
-    
-   await db.ref( 'contacts' ).on( 'value', ( snapshot ) => {
-        const data = snapshot.val();
-      
-       res.render( 'index', {contacts : data} );
+router.get( '/', async ( req, res ) => {
+
+  var query = await db.ref( 'contacts' ).orderByKey();
+  
+  query.once( "value" )
+    .then( function ( snapshot ) {
+      const contacts = [];
+      snapshot.forEach( function ( child ) {
+        var pkey = child.key;
+        var cval = child.val();
+
+        cval.key = pkey
+        contacts.push( cval )
+
+      });
+     
+      res.render( 'index', {contacts : contacts});
     })
-   
+  
+    
+  
 } );
 
 router.post( "/new-contact", async ( req, res ) => {
-  const newContact = { firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email, phone: req.body.phone };
+  const newContact = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    phone: req.body.phone
+  };
 
   await db.ref( 'contacts' ).push( newContact );
   res.redirect( "/" );
 } );
+
+//delete
+router.get( "/delete-contact/:key", async( req, res ) => {
+  await db.ref( 'contacts/' + req.params.key ).remove();
+  res.redirect("/")
+})
+
+//update
 
 
 module.exports = router;
